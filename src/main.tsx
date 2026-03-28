cd9e01532a5b2bb750c8745e8eaaf6fa497b0d0e
BrandonRogers <ber64111@gmail.com>
2026-03-28 13:00:02 -0600

    Update build

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='/personal-portfolio'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
